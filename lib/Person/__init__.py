4c9fab95145372c104bf1e8a12fa24614fb872eb
hawkenliu <hawkneliu@tencent.com>
2017-11-18 22:55:29 +0800

    add readme

--- lib/Person/__init__.py
+++
@@ -1,6 +0,0 @@
-class Person(object):
-    def __init__(self, name='xiaoming', gender='male', **kw):
-        self.name = name
-        self.gender = gender
-        for k,v in kw.items():
-            setattr(self, k, v)
